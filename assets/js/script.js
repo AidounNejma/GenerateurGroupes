195ded444f8e47be75dbc38160e50824dbba140e
AidounNejma <guermachenejma@gmail.com>
2021-12-08 15:57:45 +0100

    pseudoCode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
 var noms = [];
 
+var ajouter = document.querySelector('.ajouter')
+var oldInput = document.querySelector('.oldInput')
+//console.log(ajouter)
+
+ajouter.addEventListener('click', ajouterUnInput)
+
